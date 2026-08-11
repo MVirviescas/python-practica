7fccfa8bd5f8f1ac8ee33972afc04900b0252692
Miguel Angel <miguelo2308@hotmail.com>
2026-08-11 14:18:47 -0400

    Completo ejercicio 01

---
+++ ejercicio_01.py
@@ -0,0 +1,16 @@
+nombre_cliente = input("Cual es tu nombre: ")
+producto = input("Nombre del Producto: ")
+precio = int(input("Cual es el precio: "))
+cantidad = int(input("Cual es la cantidad: "))
+descuento = int(input("Cual es el descuento: ")) / 100
+
+subtotal = precio * cantidad 
+descuento_aplicado = subtotal * descuento 
+total = subtotal - descuento_aplicado
+
+print(f"Cliente: {nombre_cliente}")
+print(f"Producto: {producto}")
+print(f"Cantidad: {cantidad}")
+print(f"Subtotal: {subtotal}")
+print(f"Descuento: {descuento_aplicado}")
+print(f"Total a pagar: {total}")
