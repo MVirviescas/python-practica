6d0d4d570ea505415d00d9fb92d499d89feaa076
Miguel Virviescas <miguelo2308@hotmail.com>
2026-08-12 21:52:07 -0400

    Ejercicio 1 finalizado

--- ejercicio_04_if_elif_else.py
+++ ejercicio_04_if_elif_else.py
@@ -5,3 +5,25 @@ usuario = {
     "premium": False,
     "bloqueado": False
 }
+if (usuario["edad"] >= 18 and not usuario["bloqueado"]):
+    if usuario["premium"]:
+        print("Acceso premium")
+    else:
+        print("Acceso estándar")
+elif usuario["edad"] < 18:
+    print("Acceso denegado")            
+else:
+    print("Cuenta Bloqueada")
+
+#codigo correcto
+'''if usuario["bloqueado"]:
+    print("Cuenta bloqueada")
+
+elif usuario["edad"] >= 18:
+    if usuario["premium"]:
+        print("Acceso premium")
+    else:
+        print("Acceso estándar")
+
+else:
+    print("Acceso denegado")'''
