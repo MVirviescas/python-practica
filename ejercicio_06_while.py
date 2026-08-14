648d8a010e8ed0ca15ca166172827425470dff4b
Miguel Angel <miguelo2308@hotmail.com>
2026-08-14 16:34:54 -0400

    putos ciclos while

---
+++ ejercicio_06_while.py
@@ -0,0 +1,45 @@
+#1
+numero = 5
+
+while numero > 2:
+    print(numero)
+    numero -= 1
+
+print(f"Final: {numero}")
+
+#2
+contador = 0
+
+while contador < 3:
+    print(f"Contador: {contador}")
+    contador += 1
+
+print("Terminado")
+
+#3 epetir hasta que el usuario escriba algo válido.
+opcion = ""
+
+while opcion != "salir":
+    opcion = input("Escribe 'salir' para terminar: ")
+
+print("Programa terminado")
+
+#4 while con acumuladores
+numero = 1
+total = 0
+
+while numero <= 4:
+    total += numero
+    numero += 1
+
+print(total)
+
+#5 con resta 
+numero = 10
+total = 0
+
+while numero >= 4:
+    total += numero
+    numero -= 2
+
+print(total)
